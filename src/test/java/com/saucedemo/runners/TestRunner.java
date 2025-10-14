d5f9e7da8d620e9dd326a2f26b54188f7a28d4ce
David Susanto <david.susanto@stockbit.com>
2025-10-14 18:41:30 +0700

    update testrunner.java using allure report

--- src/test/java/com/saucedemo/runners/TestRunner.java
+++ src/test/java/com/saucedemo/runners/TestRunner.java
@@ -13,7 +13,8 @@ import org.testng.annotations.Test;
             "pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
-                "junit:target/cucumber-reports/cucumber.xml"
+                "junit:target/cucumber-reports/cucumber.xml",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         monochrome = true
 )
